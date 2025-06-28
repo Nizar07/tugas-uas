e87054715ccd55824aa19178eb03106204e5e0b3
Nizar07 <nizarbasalamah8@gmail.com>
2025-06-28 20:49:24 +0700

    tambah store, update, web.php - Nizar

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\LaporanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+    Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
+    Route::post('/laporan', [LaporanController::class, 'store'])->name('laporan.store');
+    Route::get('/laporan-harian', [LaporanController::class, 'laporanHarian'])->name('laporan.harian');
+    Route::get('/laporan-bulanan', [LaporanController::class, 'laporanBulanan'])->name('laporan.bulanan');
+    Route::patch('/laporan/verifikasi/{id}', [LaporanController::class, 'verifikasi'])->name('laporan.verifikasi');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::resource('users', UserController::class);
 });
 
 require __DIR__.'/auth.php';
